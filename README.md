f5aa116ee7b050d526db982edae33b54e3bf8a1c
Tristian Clayman <32968185+TrisClay345@users.noreply.github.com>
2025-04-22 18:02:26 +0000

    The program is complete. Working on documentation now

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# OSProj2
-Banker's algorithm for deadlock avoidance
+# Bankers Algorithm
+Banker's algorithm is an algorithm for finding a safe sequence of processes that can be completed without causing a deadlock given a certain number of allocated and available resources. 
+
+The program takes an input of a table of values, where each row corresponds to a process. The first n integers represent the number of each resource (n) that is currently allocated to the process. The second n integers represent the number of each resource that the process needs for completion. After all the processes and their corresponding information are inputted, the final row of n integers represents the currently available number of instances for each resource.
+
+When a process runs, it tries to get all the resources that it needs. If there is enough resources for the process, it completes its execution releasing all the resources it had. If there is not enough resources, it will begin to run and then need to wait for another process to free its resources before taking those to complete its execution. If this continues on long enough, it will wait on other waiting processes, and this can cause cycles of waiting programs which are stuck and cannot complete their execution. This is called deadlock. 
+
+Banker's algorithm tells you if the processes are in a safe state (whether it is possible to have all the processes run consecutively without waiting), and if it is, what order of process execution allows the unconflicted execution of all the processes.
+
+## Usage
+Note:
+By default, the number of expected processes is 5 and the number of resources is 3. If you want to change this, change the two variables at the top of banker.cpp before compilation
+
+To use this program, first compile it. 
+'''c++
+    g++ banker.cpp -o banker
+'''
+
+Then, create your text file with the correctly formatted tables (as discussed previously.)
+See the included input.txt as an example.
+
+Once that is done, run the program like so:
+'''c++
+    ./banker input.txt
+'''
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- banker.cpp
+++ banker.cpp
@@ -3,8 +3,10 @@
 #include <string>
 using std::string;
 
-int numProcess = 5;
-int numResources = 3;
+// This must be changed if one wants to input different numbers of processes
+const int numProcess = 5;
+// This must be changed if one wants to input different numbers of resources
+const int numResources = 3;
 
 int main(int argc, char* argv[]) { 
 
@@ -16,7 +18,7 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    string filename = argv[1];
+    const string filename = argv[1];
     std::ifstream fin(filename);
 
     // Represents number of available instances of each resource
@@ -30,6 +32,7 @@ int main(int argc, char* argv[]) {
     // of each resource allowed
     int max[numProcess][numResources];
 
+    // Gets table values from the inputted text file
     int input = 0;
     int processCount = 0;
     int i = 0;
@@ -39,27 +42,77 @@ int main(int argc, char* argv[]) {
         if(processCount >= numProcess) {
             available[i] = input;
             ++i;
-        } else if(i < 3) {
+        } else if(i < numResources) {
             allocated[processCount][i] = input;
             ++i;
-        } else if(i < 6){
-            max[processCount][i - 3] = input;
-            if(++i == 6) {
+        } else if(i < 2 * numResources){
+            max[processCount][i - numResources] = input;
+            if(++i == 2 * numResources) {
                 i = 0;
                 ++processCount;
             }
         }   
     }
 
+    // Populates need table
+
     int need[numProcess][numResources];
     for(int i = 0; i < numProcess; ++i) {
-        for(int k = 0; k < numResources; ++k)
+        for(int j = 0; j < numResources; ++j) {
+            need[i][j] = max[i][j] - allocated[i][j];
+        }
     }
 
+    bool finish[numProcess] = {0};
 
-    int finish[numProcess];
-    for(int& e : finish){
-        e = false;
+    // Stores safe sequence
+    int safeSeq[numProcess];
+
+    int counter = 0;
+    while(counter < numProcess) {
+        // True when process is found that can have its needs filled
+        bool found = false;
+        // Check processes one by one seeing if they can finish with the current available resources
+        for(int proc = 0; proc < numProcess; ++proc) {
+            if(!finish[proc]) {
+                int res;
+                // Check if this process can have its needs filled by what's available
+                for(res = 0; res < numResources; ++res) {
+                    // This statement is true when a procedure requires more of a resource
+                    // than is available at the moment (i.e. when it would have to wait)
+                    if(need[proc][res] > available[res]) break;
+                }
+                // If it can be, 
+                if(res == numResources) {
+                    // free the processes current resources, adding them to what's available
+                    for(int k = 0; k < numResources; ++k) {
+                        available[k] += allocated[proc][k];
+                    }
+
+                    // Add this process to the safe sequence, since it can be finished and 
+                    // released without having to wait on anything
+                    safeSeq[counter++] = proc;
+
+                    // Mark this procedure as finished
+                    finish[proc] = true;
+
+                    // A safe process has been found, so set found to true
+                    found = true;
+                }
+            }
+        }
+        // If no process can be found that can finish with the available resources,
+        // the system is not in a safe state
+        if(found == false) {
+            std::cout << "The system is not in a safe state." << std::endl;
+            break;
+        }
     }
-
+    // If the system is safe, print the safe sequence (order that processes can be completed
+    // without waiting)
+    std::cout << "The system is in a safe state, and the safe sequence is: ";
+    for(int i = 0; i < numProcess; ++i) {
+        std::cout << 'p' << safeSeq[i] << ' ';
+    }
+    std::cout << std::endl;
 }
